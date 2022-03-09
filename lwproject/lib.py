6da3aebc0d8e565401e88340710c5bc4eeffe5cb
Lauranne Fossat <laurannefossat10@gmail.com>
2022-03-09 14:42:59 -0300

    Implemented try function

--- lwproject/lib.py
+++ lwproject/lib.py
@@ -1,2 +1,8 @@
+from termcolor import colored
+
 def try_me():
-    print("Carpe diem. Seize the day, boys. Make your lives extraordinary.")
+    print(colored("Dead poets society : Carpe diem. Seize the day, boys. Make your lives extraordinary.",'blue'))
+    print(colored("Titanic : I'm the king of the world", 'green'))
+    print(colored("Forrest Gump : My mom always said life was like a box of chocolates. You never know what you're gonna get.", 'cyan'))
+    print(colored("Back to the future : When this baby hits 88 miles per hour, you're gonna see some serious shit.",'red'))
+    print(colored("Pulp fiction : That's when you know you've found somebody really special. When you can just shut the fuck up for a minute and comfortably share silence.",'magenta'))
